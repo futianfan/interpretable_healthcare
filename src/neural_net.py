54ee9c44b875b33a5c06dba9832c6e0c51e82b15
futianfan <futianfan@gmail.com>
2018-07-09 14:57:51 +0800

    rule matching

--- src/neural_net.py
+++ src/neural_net.py
@@ -1,4 +1,6 @@
-## python ./src/neural_net.py data/id2vec.txt data/tmp3 results/corels_rule_list
+'''
+python ./src/neural_net.py data/id2vec.txt data/tmp3 results/corels_rule_list
+'''
 
 from __future__ import print_function
 import sys
@@ -15,12 +17,14 @@ with open(sys.argv[1], 'r') as word2vec_file:
 
 ########################################################
 print('STEP 2. read-in data, read-in rule, assign data to rule')
-## read-in data:   data/tmp3
+##### input:   data/tmp3, results/corels_rule_list
+##### output:  dict{rule: data-sample}
+# read-in data:   data/tmp3
 f3 = lambda x:[int(i) for i in x.rstrip().split()]
 with open(sys.argv[2], 'r') as raw_data:
-	lines = raw_data.readlines()
-	data_dict = {i:f3(line) for i,line in enumerate(lines)}
-## read-in rule:   results/corels_rule_list
+	data_lines = raw_data.readlines()
+	data_dict = {i:f3(line) for i,line in enumerate(data_lines)}
+# read-in rule:   results/corels_rule_list
 f4 = lambda x: True if 'if ' in x and ' then ' in x else False
 def rule2num(string):
 	bgn = string.find('_') + 1
@@ -28,6 +32,8 @@ def rule2num(string):
 	fea_num = int(string[bgn:endn])
 	lab = 1 if 'yes' else 0
 	return fea_num,lab
+
+
 
 with open(sys.argv[3], 'r') as f_rule:
 	lines = f_rule.readlines()
@@ -37,12 +43,18 @@ with open(sys.argv[3], 'r') as f_rule:
 		bgn = line.find('({')
 		endn = line.find('})')
 		rule = line[bgn + 2:endn]
-		dic = {rule2num(j)[0]:rule2num(j)[1] for j in rule.split(',')}
-		rule_dict[indx] = dic
-
-
-
-
+		dic = {rule2num(j)[0]:rule2num(j)[1] for j in rule.split(',')}  # 1200:1, 398:1 
+		def rule_match_data(data_index):
+			line = data_dict[data_index]
+			for fea_num,lab in dic.items():
+				if fea_num not in line and lab == 1:
+					return False
+				if fea_num in line and lab == 0:
+					return False
+			return True
+		rule_dict[indx] = filter(rule_match_data, data_dict.keys())
+		print(len(rule_dict[indx]), end = ' ')
+#print(rule_dict[0])
 ## assign data to rule
 
 
