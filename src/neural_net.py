864336a7a05ba10689d94b3fcb7340330e8fd65c
futianfan <futianfan@gmail.com>
2018-07-11 22:10:05 +0800

    nnn

--- src/neural_net.py
+++ src/neural_net.py
@@ -1,5 +1,5 @@
 '''
-python ./src/neural_net.py data/id2vec.txt data/tmp3 results/corels_rule_list
+python ./src/neural_net.py data/id2vec.txt data/tmp3 results/corels_rule_list ./data/snow.Y
 '''
 
 from __future__ import print_function
@@ -11,6 +11,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import numpy as np 
+from time import time
 torch.manual_seed(1)    # reproducible
 ################################################################################################################ 
 print('STEP 1. read-in id2vec')
@@ -29,7 +30,8 @@ print('STEP 2. read-in data, read-in rule, assign data to rule')
 f3 = lambda x:[int(i) for i in x.rstrip().split()]
 with open(sys.argv[2], 'r') as raw_data:
 	data_lines = raw_data.readlines()
-	data_dict = {i:f3(line) for i,line in enumerate(data_lines)}
+	#data_dict = {i:f3(line) for i,line in enumerate(data_lines)}
+	data_dict = [f3(line) for i,line in enumerate(data_lines)]
 # read-in rule:   results/corels_rule_list
 f4 = lambda x: True if 'if ' in x and ' then ' in x else False
 def rule2num(string):
@@ -57,8 +59,15 @@ with open(sys.argv[3], 'r') as f_rule:
 				if fea_num in line and lab == 0:
 					return False
 			return True
-		rule_dict[indx] = filter(rule_match_data, data_dict.keys())
+		rule_dict[indx] = filter(rule_match_data, range(len(data_dict)))  ### data_dict.keys()
 		print(len(rule_dict[indx]), end = ' ')
+
+with open(sys.argv[4], 'r') as f_label:
+	label_line = f_label.readlines()
+	label = []
+	for line in label_line:
+		label.append(int(line))
+label = np.array(label)
 #print(rule_dict[0])
 ################################################################################################################
 print('NN training')
@@ -69,7 +78,8 @@ OUT_SIZE = 30
 NUM_LAYER = 1
 BATCH_FIRST = True 
 MAX_LENGTH = 200
-BATCH_SIZE = 32
+BATCH_SIZE = 1
+PROTOTYPE_NUM = len(rule_dict)
 
 def data2array(data_dict):
 	leng = len(data_dict)
@@ -79,9 +89,12 @@ def data2array(data_dict):
 		line = data_dict[i]
 		line = line[-MAX_LENGTH:]
 		for j in range(len(line)):
-			arr[i,j,:] = id2vec[line[j]]
+			try:
+				arr[i,j,:] = id2vec[str(line[j])]
+			except:
+				pass 
 		arr_len.append(len(line))
-	return arr 
+	return arr, arr_len
 
 class RLP(torch.nn.Module):
     def __init__(self, INPUT_SIZE, HIDDEN_SIZE, NUM_LAYER, PROTOTYPE_NUM, OUT_SIZE,  BATCH_FIRST = True):
@@ -102,15 +115,13 @@ class RLP(torch.nn.Module):
 
     def forward_rnn(self, X_batch, X_len):
     	batch_size = X_batch.shape[0]
+    	#X_batch = Variable(torch.from_numpy(X_batch).float())
     	dd1 = sorted(range(len(X_len)), key=lambda k: X_len[k], reverse = True)
         dd = [0 for i in range(len(dd1))]
         for i,j in enumerate(dd1):
             dd[j] = i
         X_len_sort = list(np.array(X_len)[dd1])
-        X_batch = torch.from_numpy(X_batch).float()
-        X_batch_v = Variable(X_batch)
-        X_batch_v = X_batch_v[dd1]
-        #X_batch_v = X_batch_v.cuda()
+        X_batch_v = X_batch[dd1]
         pack_X_batch = torch.nn.utils.rnn.pack_padded_sequence(X_batch_v, X_len_sort, batch_first=True)
         X_out, _ = self.rnn1(pack_X_batch, None)
         unpack_X_out, _ = torch.nn.utils.rnn.pad_packed_sequence(X_out, batch_first=True)
@@ -122,6 +133,8 @@ class RLP(torch.nn.Module):
 
 
     def forward(self, X_batch, X_len):
+    	X_batch = torch.from_numpy(X_batch).float()
+    	X_batch = Variable(X_batch)
     	batch_size = X_batch.shape[0]
     	X_out2 = self.forward_rnn(X_batch, X_len)
         ###### prototype
@@ -143,20 +156,68 @@ class RLP(torch.nn.Module):
     		for j in rule_dict[i]:
     			data.append(data_dict[j])
     		leng = len(data)
-    		T = int(leng / BATCH_SIZE)
+    		T = np.ceil(leng * 1.0 / BATCH_SIZE)
+    		T = int(T)
     		for j in range(T):
     			bgn = j * BATCH_SIZE
     			endn = min(leng, bgn + BATCH_SIZE)
     			batch_data, batch_len = data2array(data[bgn:endn])
+    			batch_data = Variable(torch.from_numpy(batch_data).float())
     			batch_X_out = self.forward_rnn(batch_data, batch_len)
     			if j==0:
     				X_out = batch_X_out
     			else:
     				X_out = torch.cat([X_out, batch_X_out], 0)
-    		self.prototype[i,:] = torch.mean(X_out)
-    	return
+    		self.prototype[i,:] = torch.mean(X_out, 0)
+    	#self.prototype = self.prototype.data.numpy()
+    	#self.prototype = torch.from_numpy(self.prototype).float()
 
 
+LR = 1e-3 
+nnet  = RLP(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYER, PROTOTYPE_NUM, OUT_SIZE,  BATCH_FIRST = True)
+print('Build network')
+nnet.generate_prototype(data_dict, rule_dict)
+print('Generate prototype')
+opt_  = torch.optim.SGD(nnet.parameters(), lr=LR)  # SGD    Adam 
+loss_crossentropy = torch.nn.CrossEntropyLoss()
+l_his = []
+
+N = len(data_dict)
+iter_in_epoch = int(np.ceil(N/BATCH_SIZE))
+#test_N = test_query.shape[0]
+#test_iter_in_epoch = int(test_N / batch_size)
+EPOCH = 3
+for epoch in range(EPOCH):
+    loss_average = 0
+    t1 = time()
+    for i in range(iter_in_epoch):
+        ##### train
+        print(i)
+        stt = i * BATCH_SIZE
+        endn = min(N, stt + BATCH_SIZE)
+        batch_x , batch_len = data2array(data_dict[stt:endn])
+        batch_label = label[stt:endn]
+        batch_label = torch.from_numpy(batch_label)
+        batch_label = Variable(batch_label)
+        print(batch_x.shape)
+        output = nnet(batch_x, batch_len)
+        loss = loss_crossentropy(output, batch_label)
+        opt_.zero_grad()
+        loss.backward()
+        opt_.step()
+        loss_average += loss.data[0]
+        #print('Epoch: ' + str(epoch) + ", " + str(i) + "/"+ str(iter_in_epoch)+ ': loss value is ' + str(loss.data[0]))
+        ##### train
+    l_his.append(loss_average)
+    t2 = time()
+    print('Epoch '+str(epoch) + ' takes ' + str(t2-t1) + ' seconds')
+'''
+plt.plot(l_his, label='SGD')
+plt.xlabel('iteration')
+plt.ylabel('loss')
+plt.savefig('sgd.jpg')
+plt.show()
+'''
 
 
 
