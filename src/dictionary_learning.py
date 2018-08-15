d521bd63a1afbc8f0321d4027502cac8d759e3c3
futianfan <futianfan@gmail.com>
2018-08-15 05:39:11 -0400

    dictionary learning

---
+++ src/dictionary_learning.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+
+## https://en.wikipedia.org/wiki/Sparse_dictionary_learning 
+## Suppose we have X, 
+## || X - D R ||_F^2 + \lambda || R ||_F^2
+
+## Alternative Minimize
+
+
+
+
